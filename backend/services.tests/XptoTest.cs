a8e4eaec4b2ab0d3a3ba841eb85430746c895709
Vinicius <vinicius.barbosa@rehagro.com.br>
2020-02-15 12:25:21 -0300

    corrigindo o teste

--- backend/services.tests/XptoTest.cs
+++ backend/services.tests/XptoTest.cs
@@ -8,27 +8,33 @@ using services.services.calc;
 using Moq;
 using services.services.calculos.viewModel;
 using System.Threading.Tasks;
+using entities.entity;
 
 namespace services.tests
 {
     public class XptoTest
     {
        
-        private static EFApplicationContext GetContextWithData()
-        {
-            var options = new DbContextOptionsBuilder<EFApplicationContext>()
-                              .UseInMemoryDatabase(Guid.NewGuid().ToString())
-                              .Options;
-            var context = new EFApplicationContext(options);
-            return context;
-        }
-
         [Fact]
         public void TEST_MOCK_REPOSITORY()
         {
-            var repository = new XptoRepository(GetContextWithData());
-            var all = repository.GetAll().FirstOrDefaultAsync();
-            Assert.NotNull(all);
+            var options = new DbContextOptionsBuilder<EFApplicationContext>()
+                             .UseInMemoryDatabase("portal_xpto")
+                             .Options;
+
+            using (var context = new EFApplicationContext(options))
+            {
+                context.XptoSet.Add(new Xpto { Id = Guid.NewGuid() });
+                context.XptoSet.Add(new Xpto { Id = Guid.NewGuid() });
+                context.SaveChanges();
+            }
+
+            using (var context = new EFApplicationContext(options))
+            {
+                var service = new XptoRepository(context);
+                var result = service.GetAll().ToList();
+                Assert.NotNull(result);
+            }
         }
     }
 }
